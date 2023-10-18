2cde6982d5734197a033fdc88b97e766c3394230
Gauravkr07 <gaurav.kumar@adcuratio.com>
2023-10-18 19:21:05 +0530

    Adding model for user

--- User_application/models.py
+++ User_application/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+# Model related to user
+class User_info(models.Model):
+    name = models.CharField(max_length=200, blank=False, null=False, default="user")
+    Phone = models.IntegerField(
+        blank=False, null=False, default="00000000", unique=True
+    )
+    mail = models.EmailField(
+        blank=True, null=False, unique=True, default="user@gmail.com"
+    )
+    age = models.ImageField(max_length=3, blank=True, null=True)
+
+    class Gender(models.TextChoices):
+        MEN = "Men"
+        WOMEN = "Women"
+        TRANSGENDER = "Transgender"
+
+    gender = models.CharField(choices=Gender.choices, max_length=20)
--- sawari_gari/settings.py
+++ sawari_gari/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "User_application"
+    'User_application'
 ]
 
 MIDDLEWARE = [
