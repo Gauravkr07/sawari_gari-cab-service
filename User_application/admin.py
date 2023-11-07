4eca480186812314e3a8787ea847658ef54f57bb
Gauravkr07 <gaurav.kumar@adcuratio.com>
2023-11-07 19:23:52 +0530

    change in application

--- User_application/admin.py
+++ User_application/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User_info
+from .models import User_info,Cab_driver_info
 
 admin.site.register(User_info)
+admin.site.register(Cab_driver_info)
--- User_application/views/user.py
+++ User_application/views/user.py
@@ -16,7 +16,6 @@ from django.views.decorators.csrf import csrf_exempt
 
 from User_application.models import User_info
 
-
 class User_view(View):
     @classonlymethod
     def as_view(cls, **initkwargs):
@@ -27,16 +26,15 @@ class User_view(View):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         current_url = resolve(request.path_info).url_name
-        if current_url == "Create_user" and request.method == "POST":
-            return self.create_user_info(request, *args, **kwargs)
+        if current_url == "Register_user" and request.method == "POST":
+            return self.register_user(request, *args, **kwargs)
         elif current_url == "fetch any account info" and request.method == "GET":
             return self.get_specific_user_detail(request, *args, **kwargs)
         elif current_url == "fetch all recoreds of user" and request.method == "GET":
             return self.get_specific_user_detail(request, *args, **kwargs)
 
-    async def create_user_info(self, request, *args, **kwargs):
+    async def register_user(self, request, *args, **kwargs):
         try:
-            print("heell")
             if not request.POST:
                 request_body = str_to_dict(request.body)
                 user_details = {
@@ -45,6 +43,8 @@ class User_view(View):
                     "mail": request_body.get("mail"),
                     "age": request_body.get("age"),
                     "gender": request_body.get("gender"),
+                    "is_active":request_body.get("is_active")
+
                 }
             else:
                 user_details = {
@@ -53,7 +53,10 @@ class User_view(View):
                     "mail": request.POST["mail"],
                     "age": request.POST["age"],
                     "gender": request.POST["gender"],
+                    "is_active":request.POST["is_active"]
                 }
+            if len(user_details["phone"]) != 10:
+                raise Exception("Phone number is not valid")
             user_data = await sync_to_async(
                 lambda: User_info.objects.create(**user_details)
             )()
@@ -90,6 +93,7 @@ class User_view(View):
                 "age": check_user_account.age,
                 "gender": check_user_account.gender,
                 "created_time": str(check_user_account.created_time),
+                "is_active":check_user_account.is_active
             }
             data = json.dumps(data)
             result = json.loads(data)
